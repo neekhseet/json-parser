9d2e4abf91897e991781c6c7e354c4c4fc7fd670
neekhseet <iteeshkeen@gmail.ru>
2026-07-20 17:56:45 +0500

    feat: added token and token-stream structures, planning architecture

--- includes/json.h
+++ includes/json.h
@@ -5,6 +5,36 @@ extern "C"
 {
 #endif
 
+typedef enum {
+    JSON_BEGIN_OBJECT,
+    JSON_END_OBJECT,
+    JSON_BEGIN_ARRAY,
+    JSON_END_ARRAY,
+    JSON_COLON,
+    JSON_COMMA,
+    JSON_STRING,
+    JSON_NUMBER,
+    JSON_BOOLEAN,
+    JSON_NULL
+} JsonTokens;
+
+typedef struct Token Token;
+typedef struct TokenStream TokenStream;
+
+struct Token
+{
+    JsonTokens type; 
+    char *t;
+};
+
+struct TokenStream 
+{
+    Token *tokens;
+
+    size_t capacity;
+    size_t size;
+}
+
 #ifdef __cplusplus
 }
 #endif
