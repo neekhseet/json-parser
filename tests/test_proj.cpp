effbc6b7e8c6b7a6dde8606468d2c5e2b807ee6c
neekhseet <iteeshkeen@gmail.ru>
2026-07-21 21:53:13 +0500

    refactor: del const variable with t-string

--- tests/test_proj.cpp
+++ tests/test_proj.cpp
@@ -1,11 +1,9 @@
 #include "../includes/json.h"
 #include <gtest/gtest.h>
 
-const char *t = "{";
-
 TEST(ProjTest, TokenizeRightBrace) 
 {
-    Token tok = get_token(t);
+    Token tok = get_token("{");
     EXPECT_EQ(tok.type, JSON_BEGIN_OBJECT);
     EXPECT_STREQ(tok.t, "{");
 };
